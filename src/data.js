a22b9953b90a50ab25eb2b430ded46413e40c48c
vbj1 <143013294+vbj1@users.noreply.github.com>
2026-09-02 14:24:52 +0300

    Build notary office presentation and appointment website

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const services=[
+['autentificari','Autentificări acte','Înscrisuri și acorduri pentru care legea sau părțile solicită forma autentică.'],
+['procuri','Procuri','Împuterniciri adaptate scopului și situației concrete.'],
+['declaratii','Declarații','Declarații notariale redactate în funcție de împrejurările prezentate.'],
+['contracte','Contracte','Asistență pentru pregătirea și autentificarea contractelor.'],
+['succesiuni','Succesiuni','Informații și îndrumare pentru desfășurarea procedurii succesorale.'],
+['divort','Divorț prin acord','Informații privind procedura notarială, atunci când sunt îndeplinite condițiile legale.'],
+['legalizari','Legalizări','Legalizarea copiilor de pe înscrisuri, în condițiile legii.'],
+['certificari','Certificări','Certificarea unor fapte și înscrisuri potrivit competenței notariale.'],
+['conventii','Convenții matrimoniale','Clarificarea și formalizarea regimului matrimonial ales.'],
+['testament','Testament','Redactarea și autentificarea dispozițiilor pentru cauză de moarte.'],
+['consultanta','Consultanță notarială','Clarificări inițiale privind pașii și documentele unei proceduri.'],
+['altele','Alte proceduri notariale','Solicitați biroului informații pentru situația dumneavoastră.']
+];
+export const faqs=[
+['Este necesară programarea?','Programarea este recomandată pentru a reduce timpul de așteptare și pentru a permite verificarea prealabilă a documentelor. Biroul vă va confirma solicitarea.'],
+['Ce acte trebuie să aduc?','Documentele diferă în funcție de procedură și de situația concretă. Consultați lista orientativă și contactați biroul pentru confirmare.'],
+['Pot trimite documentele înainte de programare?','Puteți solicita biroului indicații privind transmiterea prealabilă a documentelor. Nu transmiteți date sensibile înainte de a primi instrucțiuni.'],
+['Cât durează o procedură notarială?','Durata depinde de natura actului, de documentele disponibile și de verificările necesare. Veți primi o estimare după analizarea situației.'],
+['Este necesară prezența tuturor părților?','Cerințele de prezență diferă în funcție de act. Biroul vă poate comunica detaliile aplicabile cazului dumneavoastră.'],
+['Cum pot solicita informații înainte de programare?','Folosiți datele de contact care vor fi publicate de birou sau formularul de solicitare a programării.']
+];
